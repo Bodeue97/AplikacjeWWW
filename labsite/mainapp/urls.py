76394defaf66a03abfcdc37cd3b3607d43db3a3f
bohdan.lukaszewicz <bohdan.lukaszewicz@itm.com.pl>
2023-11-13 15:50:46 +0100

    Lab-5b

--- labsite/mainapp/urls.py
+++ labsite/mainapp/urls.py
@@ -1,19 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
-
-
-
 urlpatterns = [
-    path('osoby/', views.osoby),
-    path('osoba/get/<int:pk>/', views.osoba_get),
-    path('osoba/update/<int:pk>/', views.osoba_update),
-    path('osoba/delete/<int:pk>/', views.osoba_delete),
-    path('osoba/create/', views.osoba_create),
-    path('stanowiska/', views.stanowiska),
-    path('stanowisko/get/<int:pk>/', views.stanowisko_get),
-    path('stanowisko/update/<int:pk>/', views.stanowisko_update),
-    path('stanowisko/delete/<int:pk>/', views.stanowisko_delete),
-    path('stanowisko/create/', views.stanowisko_create),
-
+    path('osoby/', views.OsobyList.as_view()),
+    path('osoba/get/<int:pk>/', views.OsobaDetail.as_view()),
+    path('osoba/update/<int:pk>/', views.OsobaDetail.as_view()),
+    path('osoba/delete/<int:pk>/', views.OsobaDetail.as_view()),
+    path('osoba/create/', views.OsobyList.as_view()),
+    path('stanowiska/', views.StanowiskoList.as_view()),
+    path('stanowisko/get/<int:pk>/', views.StanowiskoDetail.as_view()),
+    path('stanowisko/update/<int:pk>/', views.StanowiskoDetail.as_view()),
+    path('stanowisko/delete/<int:pk>/', views.StanowiskoDetail.as_view()),
+    path('stanowisko/create/', views.StanowiskoList.as_view()),
 ]
--- labsite/mainapp/views.py
+++ labsite/mainapp/views.py
@@ -1,118 +1,95 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from django.db.models import Q
 from .models import Osoba, Stanowisko
 from .serializers import OsobaModelSerializer, StanowiskoModelSerializer
 
-# OSOBA
-@api_view(['GET'])
-def osoby(request):
-    search_phrase = request.GET.get('search', None)  
-
-    if search_phrase:
-        osoby = Osoba.objects.filter(Q(imie__icontains=search_phrase) | Q(nazwisko__icontains=search_phrase))
-    else:
-        osoby = Osoba.objects.all()
-
-    serializer = OsobaModelSerializer(osoby, many=True)
-    return Response(serializer.data)
-
-
-    
-
-@api_view(['GET'])
-def osoba_get(request, pk):
-    try:
-        osoba = Osoby.objects.get(pk=pk)
-    except Osoba.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    return Response(OsobaModelSerializer(osoba).data)
-
-
-@api_view(['POST'])
-def osoba_create(request):
-    serializer = OsobaModelSerializer(data = request.data)
-    if serializer.is_valid():
-        serializer.save()
+class OsobyList(APIView):
+    def get(self, request):
+        search_phrase = request.GET.get('search', None)
+        if search_phrase:
+            osoby = Osoba.objects.filter(Q(imie__icontains=search_phrase) | Q(nazwisko__icontains=search_phrase))
+        else:
+            osoby = Osoba.objects.all()
+        serializer = OsobaModelSerializer(osoby, many=True)
         return Response(serializer.data)
-    return Response(serializer.errors)
+
+    def post(self, request):
+        serializer = OsobaModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class OsobaDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Osoba.objects.get(pk=pk)
+        except Osoba.DoesNotExist:
+            raise Http404
 
-@api_view(['PUT'])
-def osoba_update(request, pk):
-     try:
-         osoba = Osoby.objects.get(pk=pk)
-     except Osoba.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-     serializer = OsobaModelSerializer(osoba, data=request.data)
-     if serializer.is_valid():
+    def get(self, request, pk):
+        osoba = self.get_object(pk)
+        serializer = OsobaModelSerializer(osoba)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        osoba = self.get_object(pk)
+        serializer = OsobaModelSerializer(osoba, data=request.data)
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['DELETE'])
-def osoba_delete(pk):
-    try:
-         osoba = Osoby.objects.get(pk=pk)
-    except Osoba.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    osoba.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#STANOWISKO
-
-@api_view(['GET'])
-def stanowiska(request):
-   
-    stanowiska = Stanowisko.objects.all()
-
-    serializer = StanowiskoModelSerializer(stanowiska, many=True)
-    return Response(serializer.data)
-
-
-    
-
-@api_view(['GET'])
-def stanowisko_get(request, pk):
-    try:
-        stanowisko = Stanowisko.objects.get(pk=pk)
-    except Stanowisko.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    return Response(StanowiskoModelSerializer(stanowisko).data)
-
-
-@api_view(['POST'])
-def stanowisko_create(request):
-    serializer = StanowiskoModelSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        osoba = self.get_object(pk)
+        osoba.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
-@api_view(['PUT'])
-def stanowisko_update(request, pk):
-    try:
-        stanowisko = Stanowisko.objects.get(pk=pk)
-    except Stanowisko.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = StanowiskoModelSerializer(stanowisko, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+
+
+class StanowiskoList(APIView):
+    def get(self, request):
+        stanowiska = Stanowisko.objects.all()
+        serializer = StanowiskoModelSerializer(stanowiska, many=True)
         return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['DELETE'])
-def stanowisko_delete(request, pk):
-    try:
-        stanowisko = Stanowisko.objects.get(pk=pk)
-    except Stanowisko.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def post(self, request):
+        serializer = StanowiskoModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    stanowisko.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+
+class StanowiskoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Stanowisko.objects.get(pk=pk)
+        except Stanowisko.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        stanowisko = self.get_object(pk)
+        serializer = StanowiskoModelSerializer(stanowisko)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        stanowisko = self.get_object(pk)
+        serializer = StanowiskoModelSerializer(stanowisko, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        stanowisko = self.get_object(pk)
+        stanowisko.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
